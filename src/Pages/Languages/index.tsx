49f20b46beb52ebbddccd4a930ea4be01933ae8c
Regimented <riku.sipoo@gmail.com>
2025-03-15 16:01:27 +0200

    add LinkCard to languages

--- src/Pages/Languages/index.tsx
+++ src/Pages/Languages/index.tsx
@@ -1,16 +1,39 @@
-import { Link } from "react-router-dom";
-import Layout from "../../components/Layout";
+import LinkCard from "../../components/LinkCard";
+import "../../styles/linkCards.css";
+
+const links = [
+  {
+    to: "/english",
+    title: "English",
+    description: "Learn about the english language",
+    imageKey: "palikat.png",
+    imageSection: "countryLanguages",
+  },
+  {
+    to: "/german",
+    title: "German",
+    description: "Learn about the german language",
+    imageKey: "redarrow.jpg",
+    imageSection: "countryLanguages",
+  },
+];
 
 function Languages() {
   return (
-    <Layout>
-      <div>
-        <h1>Languages</h1>
-        <Link to="/german">German</Link>
-        <br />
-        <Link to="/english">English</Link>
+    <div className="home-content">
+      <h1>Languages</h1>
+      <div className="link-cards-container">
+        {links.map((link) => (
+          <LinkCard
+            to={link.to}
+            title={link.title}
+            description={link.description}
+            imageKey={link.imageKey}
+            imageSection={link.imageSection}
+          />
+        ))}
       </div>
-    </Layout>
+    </div>
   );
 }
 export default Languages;
